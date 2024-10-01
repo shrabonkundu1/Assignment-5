ddef46fe82a7c83c4cfacccaabac9983e86b3c33
ShrabonKundu <shrabonkundu@gmail.com>
2024-10-01 21:32:34 +0600

    All click button function are be created

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-blog').addEventListener('click', function(){
+    window.location.href = './blog.html';
+})
+
+
+
+document.getElementById('btn-donation').addEventListener('click', function(){
+    
+    showSectionById('donation-section');
+
+})
+
+
+document.getElementById('btn-history').addEventListener('click', function(){
+    
+    showSectionById('history-section');
+
+
+})
